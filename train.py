b22697f28ce6561991f182e090d11944e4e84543
leegyuseob <guseob0701@naver.com>
2024-01-15 10:11:02 +0900

    change pseudo_labeling

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("/data/ephemeral/home/yolov8/yolo_train/yolov8m_fold43/weights/best.pt")
+model = YOLO("yolov8m.pt")
 model.train(data="/data/ephemeral/home/yolov8/ultralytics/cfg/datasets/data.yaml",
              epochs=200, 
              #cfg='/data/ephemeral/home/yolov8/ultralytics/cfg/models/v8/yolov8.yaml',
@@ -9,14 +9,9 @@ model.train(data="/data/ephemeral/home/yolov8/ultralytics/cfg/datasets/data.yaml
              multi_scale = True,
              lr0 = 0.00001,
              lrf = 0.00001,
+             mosaic= 0.5,
+             mixup= 0.7,
              optimizer = 'AdamW',
              project="yolo_train", 
-             name="yolov8m_fold43_retrain(lr0,lrf(0.00001)_epoch2002, AdamW)", 
+             name="yolov8m_AdamW_mosaic:0.5, mixup: 0.7", 
              device=0)
-
-# # 2. 훈련된 모델을 사용하여 레이블이 없는 데이터에 대한 예측을 생성합니다.
-# predictions = model.predict("/path/to/unlabeled/data")
-
-# # 3. 이 예측을 레이블로 사용하여 레이블이 없는 데이터를 레이블이 있는 데이터로 변환합니다.
-# pseudo_labeled_data = "/path/to/unlabeled/data"
-# pseudo_labeled_data.labels = predictions
